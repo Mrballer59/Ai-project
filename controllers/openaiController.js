8d952c86646e6ff94975efc95ae0062a18e34d17
B <77552838+Mrballer59@users.noreply.github.com>
2022-12-30 00:37:50 +0100

    typo

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -19,7 +19,7 @@ const generateImage = async (req, res) => {
   } catch (error) {
     if (error.response) {
       console.log(error.response.status);
-      console.log(error.response.date);
+      console.log(error.response.data);
     } else {
       console.log(error.message);
     }
